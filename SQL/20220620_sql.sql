90b0b4ba0aae4fce52ebc2a997eaa0cadea8827e
Damian Delgado <damisintesis109@gmail.com>
2022-06-20 21:59:16 -0300

    Se sube clase 2022-06-20

--- SQL/20220620_sql.sql
+++ SQL/20220620_sql.sql
@@ -1,5 +1,7 @@
 
-SHOW TABLES;
+-- Clase 2022-06-20
+
+DROP DATABASES curso_2014;
 
 SHOW TABLES;
 
@@ -65,10 +67,10 @@ INSERT INTO alumnos SET
 	tipoDocumento = "CI";
 
 INSERT INTO alumnos SET
-	documento 	= 23,
-	nombre 		= "Serafin",
-	apellido 	= "Aguila",
-	fechaNacimiento = "2003-03-18",
+	documento 	= 6559,
+	nombre 		= "Fabian",
+	apellido 	= "Maldonado",
+	fechaNacimiento = "2013-03-10",
 	tipoDocumento = "CI";
 
 INSERT INTO alumnos (documento,nombre,apellido,fechaNacimiento,tipoDocumento) VALUES
@@ -107,3 +109,104 @@ SELECT * FROM alumnos a WHERE apellido = "Delgado" ORDER BY nombre DESC LIMIT 3 
 SELECT * FROM alumnos a WHERE apellido = "Delgado" ORDER BY fechaNacimiento DESC LIMIT 8;
 
 SELECT * FROM alumnos ORDER BY tipoDocumento; 
+
+SELECT * FROM alumnos WHERE nombre LIKE ("%ian");
+
+SELECT * FROM alumnos WHERE nombre LIKE ("%ia%");
+
+SELECT * FROM alumnos WHERE nombre LIKE ("S%ia%");
+
+SELECT * FROM alumnos WHERE fechaNacimiento > "1990-01-01";
+
+SELECT * FROM alumnos WHERE fechaNacimiento > "1987-01-01" AND apellido = "delgado";
+
+SELECT * FROM alumnos WHERE fechaNacimiento > "1985-01-01" AND fechaNacimiento < "2000-06-16";
+
+SELECT * FROM alumnos a WHERE fechaNacimiento BETWEEN "1985-01-01" AND "2010-06-16";
+
+SELECT * FROM alumnos a WHERE fechaNacimiento 
+		BETWEEN "1985-01-01" AND "2010-06-16"
+		ORDER BY fechaNacimiento ASC;
+
+SELECT * FROM alumnos a WHERE fechaNacimiento 
+		BETWEEN "1985-01-01" AND "2010-06-16"
+		ORDER BY apellido ASC;
+	
+SELECT * FROM alumnos a WHERE fechaNacimiento 
+		BETWEEN "1985-01-01" AND "2010-06-16"
+		ORDER BY apellido DESC,nombre ASC;
+
+	
+SELECT documento, nombre, apellido FROM alumnos a;
+	
+SELECT documento AS numero_documento, nombre, apellido FROM alumnos a;
+	
+SELECT tipoDocumento FROM alumnos a GROUP BY tipoDocumento;
+	
+SELECT apellido FROM alumnos a GROUP BY apellido;	
+
+SELECT nombre,apellido FROM alumnos a GROUP BY apellido,nombre;	
+
+SELECT * FROM alumnos;
+
+SELECT count(*) AS total FROM alumnos;
+
+SELECT * FROM alumnos WHERE apellido LIKE ("%del%");
+
+SELECT count(*) AS total FROM alumnos WHERE apellido LIKE ("%del%");
+
+SELECT count(nombre) AS total FROM alumnos WHERE apellido LIKE ("%del%");
+
+SELECT tipoDocumento, count(tipoDocumento) AS total 
+		FROM alumnos a 
+		GROUP BY tipoDocumento;
+
+SELECT * FROM alumnos WHERE fechaNacimiento like ("1987%");	
+	
+SELECT * FROM alumnos;	
+
+UPDATE; alumnos SET
+	nombre = "Jimena"
+	WHERE documento = 2659855;
+
+UPDATE; alumnos SET
+	nombre = "Jimena",
+	apellido = "Gonzalez"
+	WHERE documento = 2659855;
+
+DELETE; FROM alumnos WHERE documento = 2659855;
+
+TRUNCATE; TABLE alumnos;
+
+
+SELECT * FROM alumnos a WHERE apellido != "delgado";
+
+SELECT * FROM alumnos a WHERE apellido NOT LIKE "%ado";
+
+SELECT * FROM alumnos a WHERE apellido = "tavarez" OR apellido = "Maldonado";
+
+SELECT * FROM alumnos a WHERE apellido != "tavarez" AND apellido != "Maldonado";
+
+SELECT * FROM alumnos WHERE apellido IN ("tavarez","MAldonado","delgado");
+
+SELECT * FROM alumnos WHERE apellido NOT IN ("tavarez","MAldonado","delgado");
+
+
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+SELECT [Campos] 
+	FROM [tabla]
+	JOIN [tabla]
+	WHERE [Condiociones]
+    GROUP BY [campos]
+	ORDER BY [campos]
+	LIMIT [Punto de salida, cantidad de registro]
+	HAVING [Filtros despues del resultado de los campos]
+
+
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
