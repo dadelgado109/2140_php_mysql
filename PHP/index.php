f3342da7afc823d368a88b5986df63f661044ea8
Damian Delgado <damisintesis109@gmail.com>
2022-07-11 20:18:43 -0300

    Se actualiza la clase

--- PHP/index.php
+++ PHP/index.php
@@ -1,9 +1,16 @@
 <?php
 
 	$numero = 15;
-
+	var_dump($numero);
+	echo("<br>");
 	$texto = "Soy un texto";
 	var_dump($texto);
+	echo("<br>");
+	$bolean = False;	
+	var_dump($bolean);
+	print_r("<br>");
+	$array = array();
+	var_dump($array);
 
 ?>
 <!DOCTYPE html>
