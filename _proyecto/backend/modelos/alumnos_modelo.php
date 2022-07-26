287d07b80e6b1b8c0141909b8f2f23a61533c99a
Damian Delgado <damisintesis109@gmail.com>
2022-07-25 22:01:14 -0300

    Se actualiza clase 20220725

--- _proyecto/backend/modelos/alumnos_modelo.php
+++ _proyecto/backend/modelos/alumnos_modelo.php
@@ -13,7 +13,27 @@
 
 		protected $fechaNacimiento;
 	
-		private $totalEnLista = 3;
+		protected $estado;
+
+		private $totalEnLista = 6;
+
+
+		public function obtenerDocumento(){
+			return $this->documento;	
+		}
+		public function obtenerNombre(){
+			return $this->nombre;	
+		}
+		public function obtenerApellido(){
+			return $this->apellido;	
+		}
+		public function obtenerFechaNacimiento(){
+			return $this->fechaNacimiento;	
+		}
+		public function obtenerTipoDocumento(){
+			return $this->tipoDocumento;	
+		}
+
 
 		public function constructor($data = array()){
 
@@ -28,12 +48,33 @@
 
 		public function ingresar(){
 
+			$arrayRespuesta = array("codigo"=>"", "mensaje"=>"");
+			$sqlDuplicado = "SELECT count(*) AS total FROM alumnos WHERE documento = :documento";
+			$arrayDuplicado = array("documento" => $this->documento);
+			$lista = $this->traerListado($sqlDuplicado, $arrayDuplicado);
+			$totalRegistros = $lista[0]['total'];
+			
+			if($totalRegistros > 0){
+				$arrayRespuesta['codigo'] = "Error";
+				$arrayRespuesta['mensaje'] = "Error el documento ya se encuentra registrado";
+				return $arrayRespuesta;
+			}
+
+			if(strlen($this->documento) < 5 || strlen($this->documento) > 10){
+				$arrayRespuesta['codigo'] = "Error";
+				$arrayRespuesta['mensaje'] = "El documento tiene que ser mayor a 5 digitos y menos a 10 digitos";
+				return $arrayRespuesta;
+			}
+
+
+
 			$sql = "INSERT INTO alumnos SET
 						documento 	= :documento,
 						nombre 		= :nombre,
 						apellido	= :apellido,
 						fechaNacimiento = :fechaNacimiento,
 						tipoDocumento = :tipoDocumento;
+						estado 		= 1;
 			";
 			$arrayDatos = array(
 
@@ -46,7 +87,7 @@
 			);
 			$respuesta = $this->ejecutarConsulta($sql, $arrayDatos);
 
-			$arrayRespuesta = array("codigo"=>"", "mensaje"=>"");
+			
 			if($respuesta){
 				$arrayRespuesta['codigo'] = "OK";
 				$arrayRespuesta['mensaje'] = "Se ingreso el Alumno correctamente";
@@ -59,11 +100,45 @@
 
 		}
 
+		public function cargar($documento){
+			
 
+			$sql = "SELECT * FROM alumnos WHERE documento = :documento";
+			$arrayDatos = array("documento" => $documento);
+			$lista = $this->traerListado($sql, $arrayDatos);
+
+			if(isset($lista[0])){
+				$this->documento 		= $lista[0]['documento'];
+				$this->nombre 			= $lista[0]['nombre'];
+				$this->apellido 		= $lista[0]['apellido'];
+				$this->tipoDocumento 	= $lista[0]['tipoDocumento'];
+				$this->fechaNacimiento 	= $lista[0]['fechaNacimiento'];	
+				$this->estado 			= $lista[0]['estado'];	
+			}
+
+		}
+
+
+		public function borrar(){
+			
+			$sql = "UPDATE alumnos SET estado = 0 WHERE documento = :documento";
+			$arrayDatos = array("documento" => $this->documento);
+			$respuesta = $this->ejecutarConsulta($sql, $arrayDatos);
+			
+			if($respuesta){
+				$arrayRespuesta['codigo'] = "OK";
+				$arrayRespuesta['mensaje'] = "Se borro el Alumno correctamente";
+			}else{
+				$arrayRespuesta['codigo'] = "Error";
+				$arrayRespuesta['mensaje'] = "Error al borrar el Alumno";
+			}
+			return $arrayRespuesta;
+
+		}
 
 		public function listar($filtros = array()){
 			
-			$sql = "SELECT * FROM alumnos";
+			$sql = "SELECT * FROM alumnos WHERE estado = 1 ";
 
 			// SELECT * FROM alumnos LIMIT 0,3
 			// SELECT * FROM alumnos LIMIT 3,3
@@ -89,7 +164,14 @@
 
 		}
 
+		public function listaTipoDocumento(){
 
+			$arrayTipoDocumento = array();
+			$arrayTipoDocumento['CI'] = "CI";
+			$arrayTipoDocumento['Pasaporte'] = "Pasaporte";
+			return $arrayTipoDocumento;
+
+		}
 
 		public function traerListado($sql, $arrayData = array()){
 			/*
--- _proyecto/backend/vistas/alumnos_vista.php
+++ _proyecto/backend/vistas/alumnos_vista.php
@@ -22,6 +22,14 @@
 
 	}	
 
+	if(isset($_POST["accion"]) && $_POST['accion'] == "borrar" && isset($_POST["documento"]) && $_POST['documento'] != ""){
+
+		$documento = $_POST['documento'];
+		$objAlumnos->cargar($documento);
+		$respuesta = $objAlumnos->borrar();
+
+	}
+
 
 	// 
 	$totalMaximo = $objAlumnos->totalPaginas();
@@ -55,7 +63,7 @@
 
 	$arrayFiltros = array("pagina"=>$pagina-1);
 	$listaAlumnos = $objAlumnos->listar($arrayFiltros);
-
+	$listaPasaporte = $objAlumnos->listaTipoDocumento(); 
 ?>
 <h1>Alumnos</h1>
 <div>
@@ -93,8 +101,15 @@
 					<div class="input-field col s6">
 						<select name="txtTipoDocumento">
 							<option value="">Seleccioens una opcion</option>
-							<option value="CI">CI</option>
-							<option value="Pasaporte">Pasaporte</option>
+<?php 				foreach($listaPasaporte as $clave => $valor){
+
+?>
+						<option value="<?=$clave?>"><?=$valor?></option>
+<?PHP
+					}
+?>
+
+
 						</select>
 						<label for="apellido">Tipo Documento</label>
 					</div>
@@ -134,6 +149,80 @@
 ?>
 
 
+<?PHP 
+	if(isset($_GET['accion']) && $_GET['accion'] == "editar" && isset($_GET['alumno']) && $_GET['alumno'] != ""  ){
+		$objAlumnos->cargar($_GET['alumno']);
+
+?>
+	<div class="grey lighten-3 center-align">	
+		<h3>Editar Alumno</h3>
+		<form action="index.php?r=<?=$rutaPagina?>" method="POST" class="container col s10">
+			<div class="row">
+				<div class="input-field col s12">
+					<input placeholder="Documento" id="documento" type="text" class="validate" name="txtDocumento" value="<?=$objAlumnos->obtenerDocumento()?>">
+					<label for="documento">Documento</label>
+				</div>
+			</div>
+			<div class="row">
+				<div class="input-field col s6">
+					<input placeholder="Nombre" id="nombre" type="text" class="validate" name="txtNombre" value="<?=$objAlumnos->obtenerNombre()?>">
+					<label for="nombre">Nombre</label>
+				</div>
+				<div class="input-field col s6">
+					<input placeholder="Apellido" id="apellido" type="text" class="validate" name="txtApellido" value="<?=$objAlumnos->obtenerApellido()?>">
+					<label for="apellido">Apellido</label>
+				</div>
+			</div>
+			<div class="row">
+				<div class="input-field col s6">
+					<input placeholder="Fecha Nacimiento" id="fechaNacimiento" type="date" class="validate" name="txtFechaNacimiento" value="<?=$objAlumnos->obtenerFechaNacimiento()?>">
+					<label for="fechaNacimiento">Fecha Nacimiento</label>
+				</div>
+				<div class="input-field col s6">
+					<select name="txtTipoDocumento">
+						<option value="">Seleccioens una opcion</option>
+<?php 				foreach($listaPasaporte as $clave => $valor){
+
+?>
+					<option value="<?=$clave?>"><?=$valor?></option>
+<?PHP
+				}
+?>
+
+
+					</select>
+					<label for="apellido">Tipo Documento</label>
+				</div>
+			</div>			
+			<button class="btn waves-effect waves-light" type="submit" name="accion" value="ingresar">Enviar
+				<i class="material-icons right">send</i>
+			</button>
+		</form>
+	
+	</div>
+<?php
+	}
+?>
+
+<?PHP 
+	if(isset($_GET['accion']) && $_GET['accion'] == "borrar" && isset($_GET['alumno']) && $_GET['alumno'] != ""  ){
+?>
+	<div class="grey lighten-3 center-align">	
+		<form action="index.php?r=<?=$rutaPagina?>" method="POST" class="col s12">
+			<h3>Borrar Alumno</h3>
+			<h4>Desa borra al alumno <?=$_GET['alumno']?></h4>
+			<input type="hidden" name="documento" value="<?=$_GET['alumno']?>">
+			<button class="btn waves-effect waves-light red" type="submit" name="accion" value="borrar">Eliminar
+				<i class="material-icons right">deleted</i>
+			</button>
+			<button class="btn waves-effect waves-light blue" type="submit" name="accion" value="cancelar">Cancelar
+				<i class="material-icons right">cancel</i>
+			</button>		
+		</form>
+	</div>
+<?php
+	}
+?>
 
 <table class="striped">
 	<thead>
@@ -159,10 +248,10 @@
 			<td class="center"><?=$alumno['tipoDocumento']?></td>
 			<td>
 				<div class="right">
-					<a class="waves-effect waves-light btn indigo darken-3">
+					<a href="index.php?r=<?=$rutaPagina?>&accion=editar&alumno=<?=$alumno['documento']?>" class="waves-effect waves-light btn indigo darken-3">
 						<i class="material-icons left">edit</i>
 					</a>
-					<a class="waves-effect waves-light btn red">
+					<a href="index.php?r=<?=$rutaPagina?>&accion=borrar&alumno=<?=$alumno['documento']?>" class="waves-effect waves-light btn red">
 						<i class="material-icons left">delete</i>
 					</a>
 				<div>
